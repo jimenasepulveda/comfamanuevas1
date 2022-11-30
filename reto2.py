6200e4564d227848f991f156b4d637d0cc19ca38
Jimena Sepulveda Garcia <JimenaSepulveda@comfama.com.co>
2022-11-30 10:56:52 -0500

    se crea ejemplo de condiciones multiples

---
+++ reto2.py
@@ -0,0 +1,12 @@
+mes=input("Digita el mes en el que estás: ")
+
+if mes =="marzo" or mes =="abril" or mes =="mayo":
+    print("estas en primavera")
+elif mes =="junio" or mes =="julio" or mes =="agosto":
+    print ("estas en verano")
+elif mes =="septiembre" or mes =="octubre" or mes =="noviembre":
+    print ("estas en otoño")
+elif mes =="diciembre" or mes =="enero" or mes =="febrero":
+    print ("estas en invierno")
+else: 
+    print ("Vuelva a digitar el mes")
---
+++ seleccionandodatos.py
@@ -0,0 +1,12 @@
+#Entradas del problema 
+niveldeagua=int(input("digita el nivel de agua"))
+
+#Eveluando caminos multiples (mas de 2)
+if niveldeagua<=100:
+    print("bajo nivel de agua")
+elif niveldeagua >100 and niveldeagua<400:
+    print("operacion optima")
+elif niveldeagua >=400:
+    print("peligro...")
+else: 
+    print("nivel de agua no valido")
